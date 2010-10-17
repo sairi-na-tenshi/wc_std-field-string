8f3c25aac097c8dee840abfadecc63bea33bebdf
tenshi <sairi.na.tenshi@gmail.com>
2010-10-17 20:36:51 +0400

    master

--- init.cmd
+++ init.cmd
@@ -1,1 +1,1 @@
-git init && git pull git://github.com/sairi-na-tenshi/wc_std-field-string.git && git submodule init && git submodule update
+git init && git pull git://github.com/sairi-na-tenshi/wc_std-field-string.git master && git submodule init && git submodule update
